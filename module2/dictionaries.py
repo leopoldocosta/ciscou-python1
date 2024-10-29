918304d6733dd68f5e92f5295041e3e8b440f693
leopoldocosta <leopoldocosta@gmail.com>
2024-10-28 22:24:17 -0300

    Study notes for module 2

---
+++ module2/dictionaries.py
@@ -0,0 +1,19 @@
+import netmiko 
+
+csr1kv1_connection_info = {
+    'ip' : '10.254.0.1',
+    'device_type' : 'cisco_ios',
+    'username' : 'cisco',
+    'password' : 'cisco',
+    'port' : 22
+}
+
+net_connect = netmiko.ConnectHandler( 
+    ip=csr1kv1_connection_info['ip'],  
+    device_type=csr1kv1_connection_info['device_type'], 
+    username=csr1kv1_connection_info['username'],  
+    password=csr1kv1_connection_info['password'],  
+    port=csr1kv1_connection_info['port']) 
+
+print(net_connect.send_command('show run'))
+
--- module2/sets.py
+++ module2/sets.py
@@ -15,3 +15,7 @@ ip_addrs = [
 ip_addrs_unique = set(ip_addrs)
 print(ip_addrs_unique)
 print(sorted(ip_addrs_unique))
+
+# student@student-vm:~/lab_work$ python sets.py
+# {'10.254.0.3', '10.254.0.2', '10.254.0.1'}
+# ['10.254.0.1', '10.254.0.2', '10.254.0.3']
