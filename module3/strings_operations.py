b64d7f4f07a89f35ddb194aa932017ede43f4958
leopoldocosta <leopoldocosta@gmail.com>
2024-10-28 21:46:10 -0300

    Study notes for module 3

--- module3/strings_operations.py
+++ module3/strings_operations.py
@@ -2,4 +2,10 @@ connection_info = '10.254.0.1,cisco,cisco'
 
 # print(connection_info)
 connection_info_list = connection_info.split(',')
-print(connection_info_list)
+# print(connection_info_list)
+
+n = 1
+ip_three_octets = '10.254.0.'
+print(ip_three_octets + str(n))
+print(ip_three_octets + str(n + 1))
+print(ip_three_octets + str(n + 2))
